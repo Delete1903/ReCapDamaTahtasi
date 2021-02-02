cba0afaf6ba355d8e5f57a92ea5beb801fc548f8
Serkan ÖZKAN <57104731+Delete1903@users.noreply.github.com>
2021-02-02 12:32:06 +0300

    Add project files.

---
+++ ReCapDemoDama/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ReCapDemoDama
+{
+    partial class ReCapDemoDama
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ReCapDemoDama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ReCapDemoDama";
+            this.Text = "ReCap Demo Dama";
+            this.Load += new System.EventHandler(this.ReCapDemoDama_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ReCapDemoDama/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReCapDemoDama
+{
+    public partial class ReCapDemoDama : Form
+    {
+        public ReCapDemoDama()
+        {
+            InitializeComponent();
+        }
+
+        private void ReCapDemoDama_Load(object sender, EventArgs e)
+        {
+            //02,02,2021
+            //uygulama ilk açıldığında çalışacak kısım.
+
+            //her buton bir classtır.
+
+            // aynı işlemden farklı nesneler oluşturup onları kontrol altında tutmak için array kullanılır.
+
+
+            Button[,] buttons = new Button[8,8]; // button nesnesi oluşturuldu.
+
+            int top = 0;    // asagı olan uzaklık
+            int left = 0; // sola olan uzaklık
+
+            // GetUpperBound 0.boyutun en büyük alabileceği değerdir.buda 7'dir.
+            // 8 satır için bir değer oluşturduk.
+            for (int i = 0; i < buttons.GetUpperBound(0); i++) 
+            {
+                for (int j = 0; j <buttons.GetUpperBound(1) ; j++)  // her bir satır için buton oluşturulur. i=0 iken j=0 0.0 oluşturulur.
+                {
+                    buttons[i, j] = new Button(); // ilk buton bizim için yeni bir butondur.
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left; // sola olan uzaklık.
+                    buttons[i, j].Top = top; //top=0 ilk 8 buton için.
+                    left += 50; // butonları yan yana dizdi.
+
+                    if ((i+j)%2==0) // background algoritması..
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    this.Controls.Add(buttons[i, j]);  // butonu ekrana koyar.
+
+
+                }
+                top += 50; // 2.satır için top her defasında 50,50 artar.
+                left = 0; // ikinci satıra geçti.
+
+                //8*8 button arrayi oluştu.
+
+
+            }
+            
+            
+
+           
+        }
+    }
+}
